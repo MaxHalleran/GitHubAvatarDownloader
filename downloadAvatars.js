b93bb61f9fd29f6617e5faf63e52721cd2ae3dfa
Max <MaxHalleran@gmail.com>
2018-09-03 21:47:11 +0000

    Worked on Authentication keys

--- downloadAvatars.js
+++ downloadAvatars.js
@@ -3,9 +3,28 @@
 
 // We are using request because that's a project requirement and fs to be able to write files
 const request = require('request');
-const fs = require('fs');
+const gitToken = require('./secrets.js');
+//const fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    header: {
+      'User-Agent': 'request'
+    },
+    "Authorization": gitToken
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
 
 // We need to take in the name of a repo user and their repository directly from the command line
 
