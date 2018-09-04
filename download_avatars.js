3ae3b511d49cb07ed3e39d280f64a0d588ba3b8d
Max <MaxHalleran@gmail.com>
2018-09-04 01:14:32 +0000

    Deleted the pointless second file and added dotenv functionality

--- download_avatars.js
+++ download_avatars.js
@@ -1,46 +1,46 @@
+//Initialize all the modules and information needed throughout the program
+const dotenv = require('dotenv').config;
 const request = require('request');
 const fs = require('fs');
 const token = require('./secrets.js');
 const args = process.argv;
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': token.GITHUB_TOKEN
+      'Authorization': dotenv.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
     cb(err, JSON.parse(body));
   });
-
 }
 
-function checkDirectory(directory, callback) {
-  fs.stat(directory, function(err, stats) {
-    //Check if error defined and the error code is "not exists"
-    if (err && err.errno === 34) {
-      //Create the directory, call the callback.
-      fs.mkdir(directory, callback);
-    } else {
-      //just in case there was a different error:
-      callback(err);
-    }
-  });
-}
+// function checkDirectory(directory, callback) {
+//   fs.stat(directory, function(err, stats) {
+//     //Check if error defined and the error code is "not exists"
+//     if (err && err.errno === 34) {
+//       //Create the directory, call the callback.
+//       fs.mkdir(directory);
+//     } else {
+//       //just in case there was a different error:
+//       callback(err);
+//     }
+//   });
+// }
 
 function downloadImageByURL(url, filePath) {
 
-  checkDirectory('./avatars', function(error) {
-    if(error) {
-      console.log("oh no!!!", error);
-    } else {
-      //Carry on.
-    }
-  });
+  // checkDirectory('./avatars', function(error) {
+  //   if(error) {
+  //     console.log("oh no!!!", error);
+  //   } else {
+  //     //Carry on.
+  //   }
+  // });
 
   //Get request at https://sytantris.github.io/http-examples/future.jpg
   request.get(url)
@@ -49,8 +49,7 @@ function downloadImageByURL(url, filePath) {
          .on('error', function(err) {
           throw err;
          })
-         //on initial response back from the server
-         //respond with the status code, message and the content type.
+         //on initial response back from the server, respond with the status code, message and the content type.
          //And let the user know that the image is downloading
          .on('response', function (response) {
           console.log("Response Status Code: ", response.statusCode);
@@ -68,6 +67,7 @@ function downloadImageByURL(url, filePath) {
          .pipe(fs.createWriteStream(filePath));
 }
 
+//
 getRepoContributors(args[2], args[3], function(err, result) {
   if (process.argv.length < 4) {
     console.log("Error. Please specify both the user and the repo");
